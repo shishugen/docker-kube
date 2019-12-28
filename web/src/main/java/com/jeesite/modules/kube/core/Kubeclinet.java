5c9710b9b4eb8c20a548d3b01b3c2f9a7f836adf
shishugen <576108653@qq.com>
2019-12-28 23:09:25 +0800

    优化kube配置

--- web/src/main/java/com/jeesite/modules/kube/core/Kubeclinet.java
+++ web/src/main/java/com/jeesite/modules/kube/core/Kubeclinet.java
@@ -4,6 +4,13 @@ import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.utils.Utils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 /**
  * @ClassName Kubeclinet
@@ -11,19 +18,31 @@ import io.fabric8.kubernetes.client.KubernetesClient;
  * @Author ssg
  * @Date 2019/12/16 16:29
  */
-public class Kubeclinet {
+public class Kubeclinet{
     private static KubernetesClient client = null;
-    public static KubernetesClient getKubeclinet(){
-        if (client == null){
-            synchronized (Kubeclinet.class){
-                if (client == null){
-                    Config config = new ConfigBuilder()
-                            .withMasterUrl("https://192.168.103.234:6443")
-                            .build();
-                     client = new DefaultKubernetesClient(config);
-                }
-            }
+    private final static  String KUBECONFIG_FILE = Kubeclinet.filePath(Kubeclinet.class.getResource("/config/kube-config"));
+
+    public static String filePath(URL path) {
+        try {
+            return Paths.get(path.toURI()).toString();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
+    }
+        public static KubernetesClient getKubeclinet(){
+        Config config = null;
+                if (config == null) {
+                    synchronized (Kubeclinet.class) {
+                        System.setProperty(Config.KUBERNETES_KUBECONFIG_FILE, KUBECONFIG_FILE);
+                        config = new ConfigBuilder()
+                               // .withMasterUrl("https://192.168.152.132:6443")
+                                .build();
+                    }
+                }
+                     client = new DefaultKubernetesClient(config);
         return client;
     }
+
+
+
 }
